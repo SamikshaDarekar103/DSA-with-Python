8255e8f86497cca84dfeda15815685293dcc7e39
Samiksha Darekar <darekarsamiksha78@gmail.com>
2025-08-07 23:31:35 +0530

    Add files via upload

---
+++ DSA/Bubble Sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    for n in range(len(arr) - 1, 0, -1):
+        swapped = False
+        for i in range(n):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+        if not swapped:
+            break
+
+arr = list(map(int, input("Enter the array: ").split()))
+print("Unsorted list is:")
+print(arr)
+bubble_sort(arr)
+print("Sorted list is:")
+print(arr)
